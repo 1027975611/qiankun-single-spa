ea8af0a957d106b3032a68accf70bca7f51adc7f
sangyou <10279754611@qq.com>
2022-09-01 17:46:42 +0800

    // Vue2 为jsonpFunction

--- sub-vue3/vue.config.js
+++ sub-vue3/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const { name } = require('./package');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "//localhost:7402",   // 保证子应用静态资源都像是20000端口上发送的
@@ -11,7 +12,8 @@ module.exports = defineConfig({
   configureWebpack: { //需要获取打包的内容  systemjs=》 umd格式
     output: {
       libraryTarget: 'umd',
-      library: 'm-vue'
+      library: `${name}-[name]`,
+      chunkLoadingGlobal: `webpackJsonp_${name}`,// Vue2 为jsonpFunction
     }
   }
 })
