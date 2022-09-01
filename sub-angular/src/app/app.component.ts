ad928aae24d1098fa87c8b7f5ea4cd4aafd6f8d3
sangyou <10279754611@qq.com>
2022-09-01 17:29:32 +0800

    console.log('卸载angular');

--- sub-angular/src/app/app.component.ts
+++ sub-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app1-root', // 此处不能和基座项目的根组件选择器相同
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sub-angular';
+  ngOnInit(){
+
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log('卸载angular');
+
+  }
 }
