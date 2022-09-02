5734be3e2ab2555928e89e32964ef9db77dde22a
sangyou <10279754611@qq.com>
2022-09-02 16:27:23 +0800

    init:  sub-angular (打包配置)

--- sub-angular/extra-webpack.config.js
+++ sub-angular/extra-webpack.config.js
@@ -1,8 +1,20 @@
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+const { merge } = require('webpack-merge');
+const { name } = require('./package');
+
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
-  // Feel free to modify this webpack config however you'd like to
-  return singleSpaWebpackConfig;
+  const singleSpaConfig = {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+    },
+    externals: {
+      'zone.js': 'Zone',
+    },
+  };
+  const mergedConfig = merge(singleSpaWebpackConfig, singleSpaConfig);
+  return mergedConfig;
 };
