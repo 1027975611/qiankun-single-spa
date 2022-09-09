4d51e6131a5d5becbe367f084f8efb3de99faed9
sangyou <10279754611@qq.com>
2022-09-09 16:13:01 +0800

    每个 Angular 应用程序都应该在一个单独的 repo 中并单独部署

--- main/src/global-configuration.ts
+++ main/src/global-configuration.ts
@@ -5,18 +5,19 @@ type SUBAPP ={
   SUB_ANGULAR: string | null,
 }
 // 采用 同一域名 下时  此时需要设置微应用构建时的 publicPath 和 history 模式的路由 base，然后才能打包放到对应的目录里。
+// 每个 Angular 应用程序都应该在一个单独的 repo 中并单独部署
 const SUBAPPALL = {
   LOCAL: {
     SUB_VUE: '//localhost:7402/subapp/sub-vue3/' ,
     SUB_REACT: '//localhost:7403/subapp/sub-react/',
     SUB_HTML: '//localhost:7404',
-    SUB_ANGULAR: '//localhost:7401/subapp/sub-angular/',
+    SUB_ANGULAR: '//localhost:7401/',
   },
   TEST: {
     SUB_VUE: '//qiankun.com/subapp/sub-vue3/' ,
     SUB_REACT: '//qiankun.com/subapp/sub-react/',
     SUB_HTML: '//qiankun.com/subapp/sub-html/',
-    SUB_ANGULAR: '//qiankun.com/subapp/sub-angular/',
+    SUB_ANGULAR: '//qiankun.angular.com/',
   },
   RC: {
     SUB_VUE: null,
