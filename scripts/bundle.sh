d1c191ce0834f18ee5782acb1455d2bca2e583f2
sangyou <10279754611@qq.com>
2022-09-23 13:06:52 +0800

    子应用独立仓库–聚合管理

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -6,16 +6,16 @@ mkdir ./dist
 mkdir ./dist/subapp
 
 # sub-angular子应用
-cp -r ./sub-angular/dist/ ./dist/subapp/sub-angular/
+cp -r ./subapp/sub-angular/dist/ ./dist/subapp/sub-angular/
 
 # sub-react子应用
-cp -r ./sub-react/build/ ./dist/subapp/sub-react/
+cp -r ./subapp/sub-react/build/ ./dist/subapp/sub-react/
 
 # sub-vue子应用
-cp -r ./sub-vue3/dist/ ./dist/subapp/sub-vue3/
+cp -r ./subapp/sub-vue3/dist/ ./dist/subapp/sub-vue3/
 
 # sub-html子应用
-cp -r ./sub-html/dist/ ./dist/subapp/sub-html/
+cp -r ./subapp/sub-html/dist/ ./dist/subapp/sub-html/
 
 # main基座
 cp -r ./main/dist/ ./dist/main
